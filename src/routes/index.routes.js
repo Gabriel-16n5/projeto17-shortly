a7dad0b11adbd74b7ddf1e89441800f261306d89
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-18 17:16:23 -0300

    criação dos routes e schemas base para construção do projeto

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,13 @@
-import { Router } from "express"
+import { Router } from "express";
+import autenticationRouter from "./autentication.routes.js";
+import rankingRouter from "./ranking.routes.js";
+import urlsRouter from "./urls.routes.js";
+import usersRouter from "./urls.routes.js";
 
 const router = Router()
+router.use(autenticationRouter);
+router.use(urlsRouter);
+router.use(usersRouter);
+router.use(rankingRouter);
 
 export default router

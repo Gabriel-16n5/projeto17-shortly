8acf6bed7c571e5e79a9d114370c0ae4fb6e5fe1
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-20 10:27:50 -0300

    implementação do login com token, completamente funcional

--- src/controllers/autenticationController.js
+++ src/controllers/autenticationController.js
@@ -29,9 +29,27 @@ export async function createAccount(req, res) {
 }
 
 export async function loginAccount(req, res) {
+    const {email, password} = req.body;
     try{
-
-        res.send("oi");
+        const account = await db.query(`
+        SELECT email, password
+            FROM users
+                Where email = $1 OR password = $2;
+        `, [email, password]);
+        if(!account.rows[0]){
+            return res.sendStatus(401);
+        } else {
+            const passwordValidation = bcrypt.compareSync(password, account.rows[0].password);
+            if(!passwordValidation) return res.sendStatus(401);
+        }
+        const token = uuid();
+        console.log({token: token});
+        const login = await db.query(`
+        UPDATE users
+            SET token = $1
+                Where email = $2;
+        `, [token, email]);
+        res.status(200).send({token: token});
     } catch (erro){
         res.send(erro.message)
     }

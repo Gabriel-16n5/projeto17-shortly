553daadd68f19a97da8b39595d069470578ea059
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-22 11:37:17 -0300

    get de todos os dados pelo usuário

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,31 @@
 import { db } from "../database/database.connection.js";
 
 export async function getusers(req, res) {
+    const {authorization} = req.headers;
+    let visitCount = 0;
+    if(!authorization) return res.status(401).send("Unauthorized access");
     try{
+        const session = await db.query(`
+        SELECT * 
+            FROM users WHERE token = $1;
+        `, [authorization]);
+        const id = session.rows[0].id
+        if(!session.rows[0]) return res.sendStatus(401);
+        const shortenedUrls = await db.query(`
+        SELECT id, "shortUrls", "urlsBase", "visitCount"
+            FROM urls WHERE "userId" = $1;
+        `, [id]);
+        for (let i = 0; i<shortenedUrls.rowCount; i++){
+            visitCount+= shortenedUrls.rows[i].visitCount;
+        }
+        const data = {
+            id: id,
+            name: session.rows[0].name,
+            visitCount,
+            shortenedUrls: shortenedUrls.rows
 
-        res.send("oi");
+        }
+        res.status(200).send(data);
     } catch (erro){
         res.send(erro.message)
     }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import autenticationRouter from "./autentication.routes.js";
 import rankingRouter from "./ranking.routes.js";
 import urlsRouter from "./urls.routes.js";
-import usersRouter from "./urls.routes.js";
+import usersRouter from "./users.routes.js";
 
 const router = Router()
 router.use(autenticationRouter);
